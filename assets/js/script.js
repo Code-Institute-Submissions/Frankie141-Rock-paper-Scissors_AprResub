b8fafe51f6bbfe157d8f07382187bdd47622798b
Frankie141 <franceswatts17@gmail.com>
2022-01-27 22:15:57 +0000

    Started JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const yourMove = document.getElementById('your-move');
+const computersMove = doument.getElementById('computers-move');
+const results = doument.getElementById('results');
+const choiceOptions = document.querySelectorAll('button');
+
+choiceOptions.forEach(choiceOptions => {
+    choiceOptions.addEventListener('click', e => {
+        
+    })
+})
