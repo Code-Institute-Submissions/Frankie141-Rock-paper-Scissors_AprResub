668c7858d5a7cd26e961b590c10ba624a6771f82
Frankie141 <franceswatts17@gmail.com>
2022-01-29 22:59:32 +0000

    Player choice buttons function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-const yourMove = document.getElementById('your-move');
-const computersMove = doument.getElementById('computers-move');
-const results = doument.getElementById('results');
-const choiceOptions = document.querySelectorAll('button');
+const yourMove = document.getElementById('your-move')
+const computersMove = doument.getElementById('computers-move')
+const results = doument.getElementById('results')
+const choiceOptions = document.querySelectorAll('button')
+let playerChoice
 
-choiceOptions.forEach(choiceOptions => {
-    choiceOptions.addEventListener('click', e => {
-        
-    })
-})
+choiceOptions.forEach(choiceOptions => choiceOptions.addEventListener("click", (e) =>  {
+    playerChoice = e.target.id 
+    yourMove.innerHTML = playerChoice
+    generateComputerChoice()
+}))
+
+
