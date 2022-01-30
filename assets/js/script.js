dfe2c9d444f805b41cbe948af1a568cac137babf
Frankie141 <franceswatts17@gmail.com>
2022-01-30 00:19:46 +0000

    Added semi-colons after running validators

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,56 @@
-const yourMove = document.getElementById('your-move')
-const computersMove = document.getElementById('computers-move')
-const results = document.getElementById('results')
-const choiceOptions = document.querySelectorAll('button')
-let playerChoice
-let computersChoice
-let result
+const yourMove = document.getElementById('your-move');
+const computersMove = document.getElementById('computers-move');
+const results = document.getElementById('results');
+const choiceOptions = document.querySelectorAll('button');
+let playerChoice;
+let computersChoice;
+let result;
 
 choiceOptions.forEach(choiceOptions => choiceOptions.addEventListener('click', (e) => {
-    playerChoice = e.target.id
-    yourMove.innerHTML = playerChoice
-    generateComputersChoice()
-    getResult()
-}))
+    playerChoice = e.target.id;
+    yourMove.innerHTML = playerChoice;
+    generateComputersChoice();
+    getResult();
+}));
 
 function generateComputersChoice() {
-    const randomChoiceNumber = Math.floor(Math.random() * choiceOptions.length)
+    const randomChoiceNumber = Math.floor(Math.random() * choiceOptions.length);
     
     if (randomChoiceNumber === 1) {
-        computersChoice = 'rock'
+        computersChoice = 'rock';
     }
     if (randomChoiceNumber === 2) {
-        computersChoice = 'scissors'
+        computersChoice = 'scissors';
     }
     if (randomChoiceNumber === 3) {
-        computersChoice = 'paper'
+        computersChoice = 'paper';
     }
-    computersMove.innerHTML = computersMove
+    computersMove.innerHTML = computersMove;
 }
 
 function getResult() {
     if (computersChoice === playerChoice) {
-        result = 'Its a draw'
+        result = 'Its a draw';
     }
     if (computersChoice === 'rock' && playerChoice === "paper") {
-        result = 'You are the winner!'
+        result = 'You are the winner!';
     }
     if (computersChoice === 'rock' && playerChoice === "scissors") {
-        result = 'You lost, better luck next time!'
+        result = 'You lost, better luck next time!';
     }
     if (computersChoice === 'paper' && playerChoice === "scissors") {
-        result = 'You are the winnder!'
+        result = 'You are the winnder!';
     }
     if (computersChoice === 'rock' && playerChoice === "rock") {
-        result = 'You lost, better luck next time!'
+        result = 'You lost, better luck next time!';
     }
     if (computersChoice === 'scissors' && playerChoice === "rock") {
-        result = 'You are the winnder!'
+        result = 'You are the winnder!';
     }
     if (computersChoice === 'scissors' && playerChoice === "paper") {
-        result = 'You lost, better luck next time!'
+        result = 'You lost, better luck next time!';
     }
-    results.innerHTML = result
+    results.innerHTML = result;
 }
 
 
