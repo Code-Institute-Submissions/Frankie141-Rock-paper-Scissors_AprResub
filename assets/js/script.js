e1ea148a7a75560c50e2caee922e05ac68d66175
Frankie141 <franceswatts17@gmail.com>
2022-02-02 22:51:17 +0000

    Amend result display var

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,83 +3,70 @@
  * and possible choices
  * Setting scores 
  */  
-
- const rockButton = document.getElementById('rock');
- const paperButton = document.getElementById('paper');
- const scissorsButton = document.getElementById('scissors');
- let computerChoiceDisplay = [rockButton,paperButton,scissorsButton];
- let playerChoiceDisplay = ['rock', 'paper', 'scissors'];
- let choiceOptions = document.querySelectorAll('button');
- let playerChoice
+ const computerChoiceDisplay = document.getElementById('computer-score');
+ const playerChoiceDisplay = document.getElementById('player-score');
+ const choiceOptions = document.querySelectorAll('game-button');
+ let playerChoice;
+ let computerChoiceNumber;
 
 /* Function to start game */        
     choiceOptions.forEach(choiceOptions => 
         choiceOptions.addEventListener('click', (e) => {
             playerChoice = e.target.id;
-            playerChoiceDisplay.innerHTML = playerChoice
+            playerChoiceDisplay.innerHTML = playerChoice;
             generateComputerChoice();
             getResults();
-}));    
+})); 
 
 function generateComputerChoice() {
+    const randomChoiceNumber = Math.floor(Math.random() * choiceOptions.length);
 
- const randomChoiceNumber = Math.floor(Math.random() * choiceOptions.length);
- 
- if (randomChoiceNumber === 1) {
-     computerChoice = 'rock';
- }
+    if (randomChoiceNumber === 1) {
+        computerChoiceDisplay = 'rock';
+        }
 
- if (randomChoiceNumber === 2) {
-     computerChoice = 'scissors';
- }
+    if (randomChoiceNumber === 2) {
+        computerChoiceDisplay = 'scissors';
+        }
 
- if (randomChoiceNumber === 3) {
-     computerChoice = 'paper';
- }
-
+    if (randomChoiceNumber === 3) {
+        computerChoiceDisplay = 'paper';
+        } 
+        computerChoiceNumber.innerHTML = computerChoiceDisplay;   
 }
 
 /* Function to get results */
  function getResults() {
-    let pScore = 0;
-    let cScore = 0;
-    let result = document.getElementById('results');
-    let playerScore = document.getElementById('player-score');
-    let computerScore = document.getElementById('computer-score');
+    const resultDisplay = document.querySelector('.results');
 
     if (computerChoiceDisplay === playerChoiceDisplay) {
-        result = 'Its a tie';
+        resultDisplay.innerHTML = 'Its a tie';
         }
 
-    if (computerChoiceDisplay === 'rock' && playerChoiceDisplay === "paper") {
-        result = 'Player, you are the winner!';
-        pScore++;
-        playerScore = pScore;
+    else if (computerChoiceDisplay == 'rock')
+        if  (playerChoiceDisplay == 'paper') {
+        resultDisplay.innerHTML = 'Player, you are the winner!';
         }
 
-    if (computerChoiceDisplay === 'rock' && playerChoiceDisplay === "scissors") {
-        result = 'Computer wins, better luck next time!';
-        cScore++;
-        computerScore = cScore;
+    else if (computerChoiceDisplay == 'rock') 
+        if  (playerChoiceDisplay == 'scissors') {
+        resultDisplay.innerHTML = 'Computer wins, better luck next time!';
         }
 
-    if (computerChoiceDisplay === 'paper' && playerChoiceDisplay === "scissors") {
-        result = 'Computer wins, better luck next time!';
-        cScore++;
-        computerScore = cScore;
+    else if (computerChoiceDisplay == 'paper')
+        if  (playerChoiceDisplay == 'scissors') {
+        resultDisplay.innerHTML = 'Computer wins, better luck next time!';
         }
 
-    if (computerChoiceDisplay === 'scissors' && playerChoiceDisplay === "rock") {
-        result = 'Player, you are the winner!';
-        pScore++;
-        playerScore = pScore;
+    else if (computerChoiceDisplay == 'scissors')
+        if  (playerChoiceDisplay == 'rock') {
+        resultDisplay.innerHTML = 'Player, you are the winner!';
         }   
 
-    if (computerChoiceDisplay === 'scissors' && playerChoiceDisplay === "paper") {
-        result = 'Computer wins, better luck next time!';
-        cScore++;
-        computerScore = cScore;
+    else if (computerChoiceDisplay == 'scissors') 
+        if  (playerChoiceDisplay == 'paper') {
+        resultDisplay.innerHTML = 'Computer wins, better luck next time!';
         }
- };
+ }
 
 
