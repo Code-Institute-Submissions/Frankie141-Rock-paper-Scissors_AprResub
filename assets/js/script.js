63c76e6832c7b2ad82219aa0cc480376b559e29d
Frankie141 <franceswatts17@gmail.com>
2022-04-20 20:59:51 +0000

    Code added to fix bug for scoreboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,27 @@
 let computerChoiceDisplay = document.getElementById('computerChoice');
 const playerChoiceDisplay = document.getElementById('playerChoice');
 const choiceOptions = document.querySelectorAll('button');
-var computerScore = document.getElementById("computer-Score");
-var playerScore = document.getElementById("player-Score");
+let computerScore = document.getElementById("computer-score");
+let playerScore = document.getElementById("player-score");
+let pScore = 1;
+let cScore = 1;
 let playerChoice;
 let computerChoice;
 
 /* Function to start game */
 choiceOptions.forEach(choiceOptions =>
-    choiceOptions.addEventListener('click', (e) => {
-        document.getElementById("clear").addEventListener("click", clear);
+    choiceOptions.addEventListener('click', (e) => {(
+        document.getElementById("clear").addEventListener("click", clear));
         playerChoice = e.target.id;
         playerChoiceDisplay.innerHTML = playerChoice;
         computerChoice = e.target.id;
         computerChoiceDisplay.innerHTML = computerChoice;
-        generateComputerChoice();
-        getResults("calling getResults function");
+        generateComputerChoice();   
     }));
 
 /* Function to generate computers choice*/
 function generateComputerChoice() {
+    console.log("generateComputerChoice");
     const randomChoiceNumber = Math.floor(Math.random() * choiceOptions.length);
 
     if (randomChoiceNumber === 1) {
@@ -41,54 +43,77 @@ function generateComputerChoice() {
     }
 
     computerChoiceDisplay.innerHTML = computerChoice;
-};
+
+    getResults("calling getResults function");
+    
+}
 
 /* Function to get results */
 function getResults() {
+    console.log("getResults");
     document.getElementById("playerChoice").value = playerChoiceDisplay;
     document.getElementById("computerChoice").value = computerChoiceDisplay;
+    console.log(computerChoiceDisplay, playerChoiceDisplay);
 
     if (computerChoiceDisplay === playerChoiceDisplay) {
-        document.getElementsByClassName("winner").innerHTML = 'Its a tie';
+        console.log(computerChoiceDisplay, playerChoiceDisplay);
+        document.getElementById("winner").innerHTML = 'Its a tie';
     }
 
-    if (computerChoiceDisplay === 'rock')
+    if (computerChoiceDisplay === 'rock') {
         if (playerChoiceDisplay === 'paper') {
-            document.getElementsByClassName("winner").innerHTML = 'Player, you are the winner!';
-            playerScore.innerHTML = parseInt(playerScore.innerHTML)+1;
+            document.getElementById("winner").innerHTML = 'Player, you are the winner!';
+            pScore++;
+            console.log(pScore);
+            updateScore();
         }
-    else {
-        if (computerChoiceDisplay === 'scissors') {
-            document.getElementsByClassName('winner').innerHTML = 'Computer wins, better luck next time!';
-            computerScore.innerHTML = parseInt(computerScore.innerHTML)+1;
-        }
+    }    
+    else if (computerChoiceDisplay === 'scissors') {
+            document.getElementById('winner').innerHTML = 'Computer wins, better luck next time!';
+            cScore++;
+            console.log(cScore);
+            updateScore();
     }
-}    
-    if (computerChoiceDisplay === 'paper')
+    if (computerChoiceDisplay === 'paper') {
         if (playerChoiceDisplay === 'scissors') {
-            document.getElementsByClassName('winner').innerHTML = 'Player, you are the winner!';
-            playerScore.innerHTML = parseInt(playerScore.innerHTML)+1;
+            document.getElementById('winner').innerHTML = 'Player, you are the winner!';
+            pScore++;
+            console.log(pScore);
+            updateScore();
         }
-    else {
-        if (computerChoiceDisplay === 'rock')
-            document.getElementsByClassName('winner').innerHTML = 'Computer wins, better luck next time!';
-            computerScore.innerHTML = parseInt(computerScore.innerHTML)+1;
-        }    
-    if (computerChoiceDisplay === 'scissors')
+    }     
+    else if (computerChoiceDisplay === 'rock') {
+            document.getElementById('winner').innerHTML = 'Computer wins, better luck next time!';
+            cScore++;
+            console.log(cScore);
+            updateScore();
+    }  
+    if (computerChoiceDisplay === 'scissors') {
         if (playerChoiceDisplay === 'rock') {
-            document.getElementsByClassName('winner').innerHTML = 'Player, you are the winner!';
-            playerScore.innerHTML = parseInt(playerScore.innerHTML)+1;
-    }
-    else {
-        if (computerChoiceDisplay === 'paper')
-            document.getElementsByClassName('winner').innerHTML = 'Computer wins, better luck next time!'; 
-            computerScore.innerHTML = parseInt(computerScore.innerHTML)+1;
-        };
+            document.getElementById('winner').innerHTML = 'Player, you are the winner!';
+            pScore++;
+            console.log(pScore);
+            updateScore();
+        }
+    }    
+    else if (computerChoiceDisplay === 'paper') {
+            document.getElementById('winner').innerHTML = 'Computer wins, better luck next time!'; 
+            cScore++;
+            console.log(cScore);
+            updateScore();
+        }
+    }        
 
+function updateScore() {
+    console.log("updateScore");
+    playerScore.innerHTML = pScore;
+    computerScore.innerHTML = cScore;
+}
+    
 function clear() {
     document.getElementById('playerChoice').innerHTML = "";
     document.getElementById('computerChoice').innerHTML = "";
     document.getElementById('winner').innerHTML = "";
     document.getElementById('player-score').innerHTML = "0";
     document.getElementById('computer-score').innerHTML = "0";
-};
+}
